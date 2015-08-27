830ae234491c237c3185d19df33368c98d5f3930
NassimRahali <nassim.rahali@gmail.com>
2015-08-27 13:49:58 +0200

    Modif build.xml

---
+++ test/calculators/AllTests.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculators;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author nsm
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ATFDCalculatorTest.class, LCOM4CalculatorTest.class, TCCCalculatorTest.class, WMCCalculatorTest.class})
+public class AllTests {
+    
+}
